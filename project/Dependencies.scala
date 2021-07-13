2fe7fe89e29fac2ddeb999d006602d53be9fac6a
Barry O'Neill <barra.oneill@gmail.com>
2021-07-13 11:19:11 -0400

    fix tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,13 @@ object Dependencies {
   )
 
   val Logging = Seq(
-    libraryDependencies += "org.typelevel" %% "log4cats-slf4j" % "1.3.1"
+    libraryDependencies ++= Seq(
+      "ch.qos.logback" % "logback-classic" % "1.2.3", // logging
+      "ch.qos.logback" % "logback-core"    % "1.2.3", // logging
+      "org.slf4j"      % "jcl-over-slf4j"  % "1.7.31",
+      "org.slf4j"      % "jul-to-slf4j"    % "1.7.31",
+      "org.typelevel" %% "log4cats-slf4j"  % "1.3.1"
+    )
   )
 
   val Persistence = Seq(
